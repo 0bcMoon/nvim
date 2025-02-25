06b3d5fd89cba37c94fafd4b5f85447911567b3b
Hicham Benoukaiss <hibenouk@e1r7p12.1337.ma>
2025-02-25 13:33:12 +0000

    init

---
+++ lua/user/core/plugins/mason.lua
@@ -0,0 +1,31 @@
+local M = {
+  "williamboman/mason-lspconfig.nvim",
+  dependencies = {
+    "williamboman/mason.nvim",
+  },
+}
+
+function M.config()
+  local servers = {
+    "lua_ls",
+    "cssls",
+    "html",
+    "jsonls",
+    "clangd",
+    "jdtls"
+  }
+
+  require("mason").setup {
+    automatic_installation = false,
+    ui = {
+      border = "rounded",
+    },
+  }
+
+  require("mason-lspconfig").setup {
+    automatic_installation = false,
+    ensure_installed = servers,
+  }
+end
+
+return M
